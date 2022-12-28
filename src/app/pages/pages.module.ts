7f4a27fa78e9eacc7f4bedf43c15967a25e0b252
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-12-28 16:56:21 -0600

    Uso de atributos personalizados

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
 import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -26,8 +26,9 @@ import { SharedModule } from '../shared/shared.module';
   ],
   imports: [
     CommonModule,
+    FormsModule,
+    RouterModule,
     SharedModule,
-    RouterModule
   ]
 })
 export class PagesModule { }
