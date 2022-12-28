95abd12347ef42edf323f716e254b17ca532d5e4
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2022-12-28 17:27:44 -0600

    Crear componente incrementador

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+import { ComponentsModule } from '../components/components.module';
+
 import { PagesComponent } from './pages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
 import { Grafica1Component } from './grafica1/grafica1.component';
-import { SharedModule } from '../shared/shared.module';
+
 
 
 
@@ -27,6 +31,7 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     CommonModule,
     FormsModule,
+    ComponentsModule,
     RouterModule,
     SharedModule,
   ]
--- src/app/pages/progress/progress.component.ts
+++ src/app/pages/progress/progress.component.ts
@@ -9,24 +9,5 @@ import { Component } from '@angular/core';
 })
 export class ProgressComponent {
 
-  progreso: number = 90;
-
-  get getPorcentaje() {
-    return `${this.progreso}%`;
-  }
-
-  cambiarValor( valor: number ){
-
-    if (this.progreso >= 100 && valor >= 0) {
-      return this.progreso = 100;
-    }
-
-    if (this.progreso <= 0 && valor < 0) {
-      return this.progreso = 0;
-    }
-
-    this.progreso = this.progreso + valor;
-
-  }
 
 }
