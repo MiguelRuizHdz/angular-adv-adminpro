637f96988b0e8e7f3d046109f585aae34697bac0
MiguelRuizHdz <miguel.ruizhz@uanl.edu.mx>
2023-09-14 08:10:18 -0600

    Operador filter

--- src/app/pages/rxjs/rxjs.component.ts
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, retry, interval, take, map } from 'rxjs';
+import { Observable, retry, interval, take, map, filter } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -28,10 +28,11 @@ export class RxjsComponent {
 
   retornaIntervalo(): Observable<number> {
 
-    return interval(1000)
+    return interval(100)
             .pipe(
-              take(4),
-              map( valor => valor + 1 )
+              map( valor => valor + 1 ),
+              filter( valor => (valor % 2 === 0) ? true : false ),
+              take(10)
             );
   }
 
